1e3590d2812e161b0f11a635fbeb4289f1a24d41
niw5924 <niw5924@naver.com>
2025-02-11 22:45:34 +0900

    proj: server URL 변경

--- lib/auth/firebase_custom_token_service.dart
+++ lib/auth/firebase_custom_token_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class FirebaseCustomTokenService {
   // 백엔드 서버의 URL: 변경해야 함!
   static const String _serverUrl =
-      'http://192.168.35.85:3000/api/firebase-custom-token';
+      'http://192.168.35.3:3000/api/firebase-custom-token';
 
   // Kakao Access Token을 백엔드로 보내 Firebase Custom Token을 받아오는 메서드
   static Future<String?> getFirebaseCustomToken(String kakaoAccessToken) async {
